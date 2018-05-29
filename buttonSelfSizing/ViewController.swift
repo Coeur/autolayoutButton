8ec578bf7cb4790992b6b2457305710f8734fcf6
Antoine Cœur <coeur@gmx.fr>
2018-05-29 19:46:47 +0800

    demo of auto-sizing button

--- buttonSelfSizing/ViewController.swift
+++ buttonSelfSizing/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+/// https://stackoverflow.com/a/50575588/1033581
+class AutoLayoutButton: UIButton {
+    override var intrinsicContentSize: CGSize {
+        return titleLabel!.sizeThatFits(CGSize(width: titleLabel!.preferredMaxLayoutWidth, height: .greatestFiniteMagnitude))
+    }
+    override func layoutSubviews() {
+        titleLabel?.preferredMaxLayoutWidth = frame.size.width
+        super.layoutSubviews()
+    }
+}
